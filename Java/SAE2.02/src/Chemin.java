7aea2292aa7f99f4edb88e3881c94f7e070d342d
Sacus1 <75908397+Sacus1@users.noreply.github.com>
2022-03-25 11:14:52 +0100

    accessible chemin

--- Java/SAE2.02/src/Chemin.java
+++ Java/SAE2.02/src/Chemin.java
@@ -1,12 +1,13 @@
 public class Chemin {
     private double cout;
     private Sommet pointDest;
-
+    private boolean Accessible = true;
     public Chemin(Sommet Destination, double cout) {
         pointDest = Destination;
         this.cout = cout;
     }
-
+    public boolean EstAccessible(){return Accessible;}
+    public void RendreAccessible(boolean b){Accessible = b;}
     public Sommet GetDest() {
         return pointDest;
     }
