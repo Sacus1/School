61c1164257806f4b7c8d889cb061a6fa4dd9115b
Sacus1 <75908397+Sacus1@users.noreply.github.com>
2022-03-28 14:16:34 +0200

    Ajout min + javadoc

--- Java/SAE2.02/src/Chemin.java
+++ Java/SAE2.02/src/Chemin.java
@@ -1,18 +1,54 @@
 public class Chemin {
     private double cout;
     private Sommet pointDest;
-    private boolean Accessible = true;
+
     public Chemin(Sommet Destination, double cout) {
         pointDest = Destination;
         this.cout = cout;
     }
-    public boolean EstAccessible(){return Accessible;}
-    public void RendreAccessible(boolean b){Accessible = b;}
+
+    public Chemin() {
+        pointDest = new Sommet("");
+        cout = 0;
+    }
+
+    public String toString() {
+        return pointDest.toString() + " : " + cout;
+    }
+
+    /**
+     * @return Le sommet de destination
+     */
     public Sommet GetDest() {
         return pointDest;
     }
 
+    /**
+     * @return le cout du chemin
+     */
     public double GetCout() {
         return cout;
+    }
+
+    /**
+     * @param Le chemin a compare
+     * @return Le chemin le plus court
+     */
+    public Chemin min(Chemin c) {
+        if (c.GetCout() > cout)
+            return this;
+        return c;
+    }
+
+    /**
+     * @param Une liste de chemin
+     * @return Chemin le plus court
+     */
+    public static Chemin min(Chemin[] c) {
+        Chemin CheminPlusCourt = c[0];
+        for (Chemin chemin : c) {
+            CheminPlusCourt = CheminPlusCourt.min(chemin);
+        }
+        return CheminPlusCourt;
     }
 }
