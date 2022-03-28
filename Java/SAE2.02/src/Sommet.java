caa6ecc393b062bae0aac292b5156074e41ae3f1
Sacus1 <75908397+Sacus1@users.noreply.github.com>
2022-03-28 14:16:34 +0200

    Ajout min + javadoc

--- Java/SAE2.02/src/Sommet.java
+++ Java/SAE2.02/src/Sommet.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Sommet {
-    private ArrayList<Chemin> chemins;
+    private ArrayList<Chemin> chemins = new ArrayList<Chemin>();
     private String nom;
 
     public Sommet(String nom, Chemin[] c) {
@@ -15,15 +15,32 @@ public class Sommet {
         this(nom, new Chemin[] {});
     }
 
-    public ArrayList<Chemin> GetChemins() {
-        return chemins;
+    /**
+     * @return Tout les chemin relier a ce sommet
+     */
+    public Chemin[] GetChemins() {
+        Chemin[] c = new Chemin[] {};
+        return chemins.toArray(c);
     }
 
+    /**
+     * @param Le chemin a ajouter
+     */
     public void NewChemin(Chemin c) {
         chemins.add(c);
     }
 
+    /**
+     * @return Nom du sommet
+     */
     public String toString() {
         return nom;
+    }
+
+    public boolean equals(Object object) {
+        if (!(object instanceof Sommet))
+            return false;
+        Sommet s = (Sommet) object;
+        return nom.equals(s.toString());
     }
 }
