d8c765815e07204c341f8075de732a014483d205
Sacus1 <75908397+Sacus1@users.noreply.github.com>
2022-04-02 16:20:20 +0200

    CA FONCTIONNE !!!

--- Java/SAE2.02/src/Graphe.java
+++ Java/SAE2.02/src/Graphe.java
@@ -1,7 +1,7 @@
 public class Graphe {
     private Sommet[] s;
     private final static double INFINITY = 10000;
-    private int last = 0;
+    private int taille = 0;
 
     public Graphe(int taille) {
         s = new Sommet[taille];
@@ -21,22 +21,32 @@ public class Graphe {
         }
     }
 
+    /**
+     * @param s
+     * @throws Exception
+     */
     public void add(Sommet s) throws Exception {
-        if (this.s.length < last + 1) {
+        if (this.s.length < taille + 1) {
             throw new Exception("Graphe surchargé");
         } else
-            this.s[last++] = s;
+            this.s[taille++] = s;
     }
 
+    /**
+     * @param indice
+     */
     public void remove(int indice) {
-        for (int i = indice + 1; i < last; i++) {
+        for (int i = indice + 1; i < taille; i++) {
             s[i - 1] = s[i];
         }
-        last--;
+        taille--;
     }
 
+    /**
+     * @param sommet
+     */
     public void remove(Sommet sommet) {
-        for (int i = 0; i < last; i++) {
+        for (int i = 0; i < taille; i++) {
             if (s[i].equals(sommet)) {
                 remove(i);
                 return;
@@ -44,10 +54,17 @@ public class Graphe {
         }
     }
 
+    /**
+     * @return int
+     */
     public int length() {
-        return last;
+        return taille;
     }
 
+    /**
+     * @param indice
+     * @return Sommet
+     */
     public Sommet Get(int indice) {
         return s[indice];
     }
@@ -58,7 +75,7 @@ public class Graphe {
      * @return l'indice du sommet ou -1 si inexistant
      */
     public int Get(Sommet sommet) {
-        for (int i = 0; i < last; i++) {
+        for (int i = 0; i < taille; i++) {
             if (s[i].equals(sommet)) {
                 return i;
             }
@@ -66,28 +83,35 @@ public class Graphe {
         return -1;
     }
 
+    /**
+     * @return String
+     */
     public String toString() {
         String s = "";
-        for (int i = 0; i < last; i++) {
+        for (int i = 0; i < taille; i++) {
             s += "\n" + this.s[i].toString();
         }
         return s;
     }
 
+    /**
+     * @param Sommet1
+     * @param Sommet2
+     * @return Graphe
+     * @throws Exception
+     */
     public Graphe PlusCourtChemin(int Sommet1, int Sommet2) throws Exception {
         Graphe Sbar = new Graphe(this);
-        Graphe chemin = new Graphe(last);
-        Graphe[] cheminMin = new Graphe[last];
+        Sommet[] Prec = new Sommet[taille];
         Sommet i = s[Sommet1];
-        double coutTotal = 0;
-        double min[] = new double[last];
-        for (int j = 0; j < min.length; j++) {
-            min[j] = INFINITY;
+        double coutPrec = 0;
+        double coutSommet[] = new double[taille];
+        for (int j = 0; j < coutSommet.length; j++) {
+            coutSommet[j] = INFINITY;
         }
-        min[Get(i)] = 0;
+        coutSommet[Get(i)] = 0;
         while (Sbar.length() > 0) {
             Sbar.remove(i);
-            chemin.add(i);
             Chemin[] gamma = i.GetChemins();
             Chemin[] gamIntSbar = new Chemin[gamma.length];
             int index = 0;
@@ -99,33 +123,36 @@ public class Graphe {
             }
             for (Chemin chemin2 : gamIntSbar) {
                 if (chemin2 == (Chemin) null)
-                    continue;
-                double couts = chemin2.GetCout() + coutTotal;
+                    break;
+                double couts = chemin2.GetCout() + coutPrec;
                 int sommetIndice = Get(chemin2.GetDest());
-                if (min[sommetIndice] > couts) {
-                    min[sommetIndice] = couts;
-                    cheminMin[sommetIndice] = new Graphe(chemin, last);
-                    cheminMin[sommetIndice].add(i);
+                if (coutSommet[sommetIndice] > couts) {
+                    coutSommet[sommetIndice] = couts;
+                    Prec[sommetIndice] = i;
                 }
             }
             double mini = INFINITY;
             i = Sbar.Get(0);
-            double c = 0;
-            for (Chemin chemin2 : gamIntSbar) {
-                if (chemin2 == (Chemin) null)
-                    continue;
-                for (int j = 0; j < min.length; j++) {
-                    if (Get(chemin2.GetDest()) == j) {
-                        if (min[j] < mini) {
-                            c = chemin2.GetCout();
-                            mini = min[j];
-                            i = Get(j);
-                        }
-                    }
+            for (int j = 0; j < Sbar.length(); j++) {
+                if (Sbar.Get(j) == (Sommet) null)
+                    break;
+                int sommetIndice = Get(Sbar.Get(j));
+                if (coutSommet[sommetIndice] < mini) {
+                    mini = coutSommet[sommetIndice];
+                    i = Sbar.Get(j);
                 }
             }
-            coutTotal += c;
+            coutPrec = coutSommet[Get(i)];
+            System.out.println();
         }
-        return cheminMin[Sommet2];
+        Graphe chemin = new Graphe(taille);
+        int j = Sommet2;
+        chemin.add(Get(j));
+        while (j != Sommet1) {
+            chemin.add(Prec[j]);
+            j = Get(Prec[j]);
+        }
+
+        return chemin;
     }
 }
