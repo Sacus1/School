f1837884210a41b510c8fdb7381ccc51d73e78da
Sacus <75908397+Sacus1@users.noreply.github.com>
2022-10-04 00:40:08 +0200

    Dev orienté objet TP1

---
+++ BUT1/Java/dootp1/TestPixel.java
@@ -0,0 +1,23 @@
+
+class TestPixel
+{
+	public static void main(String[] args)
+	{
+
+				Pixel px = new Pixel(10,10);
+				System.out.println(px);
+				Pixel px2 = new Pixel();
+				System.out.println(px2);
+				Pixel px3  = new Pixel (px);
+				System.out.println(px3);
+				Pixel px4 = new Pixel (5.5,3.3);
+				System.out.println(px4);
+				Pixel px5 = new Pixel(px,px4);
+				System.out.println(px5);
+				System.out.println(px.distance2());
+				System.out.println(px.plusLoinQue(px4));
+				System.out.println(px.distance2(new Pixel(5,5)));
+				System.out.println(px.distanceManhattan(px2));
+					
+	}
+}
