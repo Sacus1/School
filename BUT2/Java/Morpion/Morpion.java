8fdda1cd714851d60ecf4294192c2c4789adeec4
sacus <sacus54500@gmail.com>
2022-10-08 23:49:18 +0200

    Added Morpion

---
+++ BUT2/Java/Morpion/Morpion.java
@@ -0,0 +1,29 @@
+public class Morpion {
+    public static void main(String[] args) {
+        Board board = new Board();
+        // while the game is not over
+        while (!board.isGameOver()){
+
+            board.callMinimax(0, 1);
+            // check if the player that just played won
+            if (board.hasXWon()){
+                System.out.println("X won");
+                board.displayBoard();
+                return;
+            }
+            board.displayBoard();
+            // take the input
+            board.takeHumanInput(2);
+            if (board.hasOWon()){
+                System.out.println("O won");
+                board.displayBoard();
+                return;
+            }
+            // display the board
+            board.displayBoard();
+        }
+        // if the game is over and no one won, it's a draw
+        System.out.println("Draw");
+    }
+
+}
---
+++ BUT2/Java/Morpion/PointsAndScores.java
@@ -0,0 +1,16 @@
+import java.awt.Point;
+
+public class PointsAndScores {
+    private int score;
+    private Point point;
+    PointsAndScores(int score, Point point) {
+        this.score = score;
+        this.point = point;
+    }
+    public int getScore() {
+        return score;
+    }
+    public Point getPoint() {
+        return point;
+    }
+}
