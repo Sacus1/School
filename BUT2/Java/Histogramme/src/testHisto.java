5448b23722403b39c77d35fc5d55cfc59962241c
Sacus <75908397+sacus1@users.noreply.github.com>
2023-01-12 12:15:08 +0100

    Convolution

--- BUT2/Java/Histogramme/src/testHisto.java
+++ BUT2/Java/Histogramme/src/testHisto.java
@@ -28,5 +28,34 @@ public class testHisto {
         im.writePGM(args[0] + " Seiller.pgm");
         histogram = new Histogram(im);
         histogram.saveHisto("histoSeiller.csv");
+        // convolution
+        im = GreyImage.loadPGM(args[0]+".pgm");
+        short[] w={(short)1,(short)1,(short)1,
+                (short)1,(short)1,(short)1,
+                (short)1,(short)1,(short)1}; // moyenne
+        Mask M = new Mask(3,3,w);
+        GreyImage img = im.convolve(M);
+        img = img.convolve(M);
+        img= img.convolve(M);
+        img = img.convolve(M);
+        img = img.convolve(M);
+        img.writePGM(args[0] + " Convolution Moyenne*5.pgm");
+
+        // convolution Sobel
+        im = GreyImage.loadPGM(args[0]+".pgm");
+        short [] w2={(short)-1,(short)-2,(short)-1,
+                (short)0,(short)0,(short)0,
+                (short)1,(short)2,(short)1};
+        Mask M2 = new Mask(3,3,w2);
+        img = im.convolve(M2);
+        img.writePGM(args[0] + " Convolution Sobel.pgm");
+        // convolution -1 16
+        im = GreyImage.loadPGM(args[0]+".pgm");
+        short [] w3={(short)-1,(short)-1,(short)-1,
+                (short)-1,(short)16,(short)-1,
+                (short)-1,(short)-1,(short)-1};
+        Mask M3 = new Mask(3,3,w3);
+        img = im.convolve(M3);
+        img.writePGM(args[0] + " Convolution nette.pgm");
     }
 }
