a7eff5812cc0faae8fd2ace40983bcfb1db1eb05
Sacus <75908397+sacus1@users.noreply.github.com>
2023-05-06 10:39:17 +0200

    yoink arche

---
+++ BUT2/Dev mobile/TP1/mariadb/cinema-01-schema.sql
@@ -0,0 +1,79 @@
+DROP SCHEMA IF EXISTS `cinema`;
+CREATE DATABASE `cinema`;
+USE `cinema`;
+
+CREATE TABLE `Personne` (
+  `id` UUID NOT NULL DEFAULT UUID(),
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `naissance` date,
+  `deces` date,
+  `artiste` varchar(30),
+  `nationalite` char(2),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE Film (
+  `id` uuid NOT NULL DEFAULT UUID(),
+  `titre` varchar(80) NOT NULL,
+  `titre_original` varchar(80),
+  `annee` integer,
+  `sortie` date,
+  `duree` integer,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE Cinema (
+  `id` integer PRIMARY KEY,
+  `nom` character varying(60),
+  `voie` character varying(60),
+	`ville` character varying(40),
+	`coordonnees` geometry,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB;
+
+CREATE TABLE Equipe (
+  `id` integer NOT NULL AUTO_INCREMENT,
+  `film` uuid NOT NULL,
+  `personne` uuid NOT NULL,
+  `role` varchar(25),
+  `alias` varchar(40),
+  PRIMARY KEY (`id`),
+  KEY `film` (`film`),
+  CONSTRAINT `Equipe_Film_fk` FOREIGN KEY (`film`) REFERENCES `Film` (`id`),
+  KEY `personne` (`personne`),
+  CONSTRAINT `Equipe_Personne_fk` FOREIGN KEY (`personne`) REFERENCES `Personne` (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `Societe` (
+  `id` uuid PRIMARY KEY,
+  `societe` varchar(30) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE `Genre` (
+  `id` integer PRIMARY KEY,
+  `genre` varchar(30) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE `Salle` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `cinema` int(11) NOT NULL,
+  `salle` varchar(20) NOT NULL,
+  `sieges` int(11) NOT NULL,
+  CONSTRAINT `Salle_ibfk_1` FOREIGN KEY (`cinema`) REFERENCES `Cinema` (`id`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `Seance` (
+  `id` int(11) PRiMARY KEY,
+  `film` uuid NOT NULL,
+  `salle` int(11) NOT NULL,
+  `seance` datetime NOT NULL,
+  CONSTRAINT `Seance_ibfk_1` FOREIGN KEY (`film`) REFERENCES `Film` (`id`),
+  CONSTRAINT `Seance_ibfk_2` FOREIGN KEY (`salle`) REFERENCES `Salle` (`id`)
+) ENGINE=InnoDB;
---
+++ BUT2/Dev mobile/TP1/mariadb/cinema-02-data.sql
@@ -0,0 +1,50 @@
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-personne.csv'
+REPLACE INTO TABLE Personne
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(id,nom,prenom,naissance,@deces,nationalite,@artiste)
+SET
+deces = NULLIF(@deces,''), artiste = NULLIF(@artiste,'');
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-film.csv'
+REPLACE INTO TABLE Film
+FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(id,titre,titre_original,annee,sortie,duree);
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-equipe.csv'
+REPLACE INTO TABLE Equipe
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(id, film, personne, role, alias);
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-etablissement.csv'
+REPLACE INTO TABLE Cinema
+FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(id,nom,voie,ville,@latitude,@longitude)
+SET
+coordonnees = POINT(@latitude, @longitude);
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-genre.csv'
+REPLACE INTO TABLE Genre
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(id,genre);
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-salle.csv'
+REPLACE INTO TABLE Salle
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/cinema-seance.csv'
+REPLACE INTO TABLE Seance
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
